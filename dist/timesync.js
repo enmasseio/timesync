7d34f870af33a3b76d2dd80ab7397ad90a9201a4
jos <wjosdejong@gmail.com>
2015-02-04 10:59:45 +0100

    Working neatly for multiple peers now.

--- dist/timesync.js
+++ dist/timesync.js
@@ -121,30 +121,43 @@ exports.create = create;
  * https://github.com/enmasseio/timesync
  */
 
+var util = _interopRequireWildcard(require("./util.js"));
+
 var stat = _interopRequireWildcard(require("./stat.js"));
 
 var emitter = _interopRequire(require("./emitter.js"));
 
 function create(options) {
   var timesync = {
+    // configurable options
     options: {
-      id: "timesync_" + Math.round(Math.random() * 100000),
-      interval: 60 * 60 * 1000, // ms
-      timeout: 10000, // ms
-      delay: 1000, // ms
-      repeat: 5,
-      peers: [],
-      now: Date.now
+      id: "timesync_" + Math.round(Math.random() * 100000), // some semi-random identifier
+      interval: 60 * 60 * 1000, // interval for doing synchronizations in ms
+      timeout: 10000, // timeout for requests to fail in ms
+      delay: 1000, // delay between requests in ms
+      repeat: 5, // number of times to do a request to one peer
+      peers: [], // uri's or id's of the peers
+      now: Date.now // function returning the system time
     },
 
-    /** @type {number} */
+    /** @type {number} The current offset from system time */
     offset: 0, // ms
 
-    /** @type {number} */
-    timeout: null,
+    /** @type {number} Contains the timeout for the next synchronization */
+    _timeout: null,
 
-    /** @type {Object.<string, function>} */
+    /** @type {Object.<string, function>} Contains a map with requests in progress */
     inProgress: {},
+
+    /**
+     * @type {boolean}
+     * This property used to immediately apply the first ever received offset.
+     * After that, it's set to false and not used anymore.
+     */
+    _isFirst: true,
+
+    /** @type {boolean} True when running the synchronization every x seconds */
+    _running: true,
 
     /**
      * Send a message to a peer
@@ -184,7 +197,7 @@ function create(options) {
      * Send a JSON-RPC message and retrieve a response
      * @param {string} to
      * @param {string} method
-     * @param {*} params
+     * @param {*} [params]
      * @returns {Promise}
      */
     rpc: function (to, method, params) {
@@ -216,13 +229,18 @@ function create(options) {
      * Docs: http://www.mine-control.com/zack/timesync/timesync.html
      */
     sync: function () {
-      timesync.emit("sync");
+      timesync.emit("sync", "start");
 
       var peers = timesync.options.peers;
       return Promise.all(peers.map(function (peer) {
         return timesync._syncWithPeer(peer);
-      })).then(function () {
-        timesync.emit("offset", timesync.offset);
+      })).then(function (offsets) {
+        if (offsets.length > 0) {
+          // take the average of all peers (excluding self) as new offset
+          timesync.offset = stat.mean(offsets);
+          timesync.emit("change", timesync.offset, "final");
+        }
+        timesync.emit("sync", "end");
       });
     },
 
@@ -234,16 +252,16 @@ function create(options) {
      * @private
      */
     _syncWithPeer: function (peer) {
-      // retrieve an offset of the peer after a delay of 1 sec
-      function sync() {
-        var delay = timesync.options.delay;
-        return wait(delay).then(function () {
-          return timesync._retrieveOffset(peer);
+      // retrieve the offset of a peer, then wait 1 sec
+      function syncAndWait() {
+        return timesync._getOffset(peer).then(function (result) {
+          return util.wait(timesync.options.delay).then(function () {
+            return result;
+          });
         });
       }
 
-      var count = timesync.options.repeat;
-      return repeat(sync, count).then(function (all) {
+      return util.repeat(syncAndWait, timesync.options.repeat).then(function (all) {
         // filter out null results
         var results = all.filter(function (result) {
           return result !== null;
@@ -263,12 +281,8 @@ function create(options) {
           return result.offset;
         });
 
-        // apply the new offset
-        if (offsets.length > 0) {
-          timesync.offset = stat.mean(offsets);
-          return timesync.offset;
-        }
-        return null;
+        // return the new offset
+        return offsets.length > 0 ? stat.mean(offsets) : null;
       });
     },
 
@@ -278,15 +292,24 @@ function create(options) {
      * @returns {Promise.<{roundtrip: number, offset: number} | null>}
      * @private
      */
-    _retrieveOffset: function (peer) {
+    _getOffset: function (peer) {
       var start = Date.now(); // local system time
 
       return timesync.rpc(peer, "ping").then(function (timestamp) {
         var end = Date.now(); // local system time
         var roundtrip = end - start;
+        var offset = timestamp - end + roundtrip / 2; // offset from local system time
+
+        // apply the first ever retrieved offset immediately.
+        if (timesync._isFirst) {
+          timesync._isFirst = false;
+          timesync.offset = offset;
+          timesync.emit("change", offset);
+        }
+
         return {
           roundtrip: roundtrip,
-          offset: timestamp - end + roundtrip / 2 // offset from local system time
+          offset: offset
         };
       })["catch"](function (err) {
         // just ignore failed requests, return null
@@ -306,11 +329,11 @@ function create(options) {
      * Start synchronizing once per interval
      */
     start: function () {
-      timesync.running = true;
+      timesync._running = true;
 
       timesync.sync().then(function () {
-        timesync.timeout = setTimeout(function () {
-          if (timesync.running) {
+        timesync._timeout = setTimeout(function () {
+          if (timesync._running) {
             timesync.start();
           }
         }, timesync.options.interval);
@@ -321,40 +344,62 @@ function create(options) {
      * Stop synchronizing once per interval
      */
     stop: function () {
-      clearTimeout(timesync.timeout);
-      timesync.timeout = null;
-      timesync.running = false;
-    },
-
-    running: false
+      clearTimeout(timesync._timeout);
+      timesync._timeout = null;
+      timesync._running = false;
+    }
   };
 
   // apply provided options
   if (options) {
     for (var prop in options) {
-      timesync.options[prop] = options[prop];
+      if (options.hasOwnProperty(prop)) {
+        timesync.options[prop] = options[prop];
+      }
     }
+  }
+
+  // validate configuration
+  if (!timesync.options.peers || timesync.options.peers.length === 0) {
+    throw new Error("No peers configured");
+  }
+  if (!Array.isArray(timesync.options.peers)) {
+    timesync.options.peers = [timesync.options.peers];
   }
 
   // turn into an event emitter
   emitter(timesync);
 
   // start a timer to synchronize once per interval
-  wait(0).then(timesync.start);
+  setTimeout(function () {
+    if (timesync._running) {
+      timesync.start();
+    }
+  }, 0);
 
   return timesync;
 }
+
+/**
+ * Simple id generator
+ * @returns {number} Returns a new id
+ */
+function nextId() {
+  return _id++;
+}
+var _id = 0;
+exports.__esModule = true;
+
+},{"./emitter.js":1,"./stat.js":2,"./util.js":4}],4:[function(require,module,exports){
+"use strict";
 
 /**
  * Resolve a promise after a delay
  * @param {number} delay    A delay in milliseconds
  * @returns {Promise} Resolves after given delay
  */
-function wait(delay) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, delay);
-  });
-}
+exports.wait = wait;
+
 
 /**
  * Repeat a given asynchronous function a number of times
@@ -362,7 +407,12 @@ function wait(delay) {
  * @param {number} times
  * @return {Promise}
  */
-function repeat(fn, times) {
+exports.repeat = repeat;
+function wait(delay) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}function repeat(fn, times) {
   return new Promise(function (resolve, reject) {
     var count = 0;
     var results = [];
@@ -382,16 +432,7 @@ function repeat(fn, times) {
     recurse();
   });
 }
-
-/**
- * Simple id generator
- * @returns {number} Returns a new id
- */
-function nextId() {
-  return _id++;
-}
-var _id = 0;
 exports.__esModule = true;
 
-},{"./emitter.js":1,"./stat.js":2}]},{},[3])(3)
+},{}]},{},[3])(3)
 });
